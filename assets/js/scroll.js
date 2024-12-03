23f19b1dd0ad93f0dab883e5f7d977eb8b791012
Martin Aguilar <martinaguilarjm@gmail.com>
2024-12-03 00:20:11 -0600

    se realizaron varios cambios en caunto al navbar

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,39 +1,27 @@
-// Función de desplazamiento suave
-function smoothScrollTo(target, duration) {
-   const start = window.scrollY || document.documentElement.scrollTop;
-   const startTime = performance.now();
-   const targetPosition = target === 'top' ? 0 : document.querySelector(target).offsetTop;
-   const distance = targetPosition - start;
-   
-   function scrollStep(currentTime) {
-     const elapsedTime = currentTime - startTime;
-     const progress = Math.min(elapsedTime / duration, 1); // Normaliza el progreso entre 0 y 1
-     const easing = 0.5 - Math.cos(progress * Math.PI) / 2; // Easing de tipo "ease-in-out"
-     window.scrollTo(0, start + distance * easing);
-     if (elapsedTime < duration) {
-       requestAnimationFrame(scrollStep);
-     }
-   }
-   requestAnimationFrame(scrollStep);
-}
 
-// Agrega evento al botón para ir al tope
-const scrollButton = document.getElementById('scrollTopButton');
-if (scrollButton) {
-   scrollButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      smoothScrollTo('top', 1000); // Cambia 1000 a la duración en milisegundos que desees
-   });
-}
+let lastScrollTop = 0; // Para evitar que el scroll rápido cause efectos erráticos
+let debounceTimer; // Timer for debouncing
 
-// Controlador para el menú de hamburguesa
-document.addEventListener('DOMContentLoaded', function () {
-   const hamburger = document.getElementById('hamburger');
-   const navLinks = document.querySelector('.nav-links');
+window.addEventListener('scroll', function () {
+   clearTimeout(debounceTimer); // Clear the previous timer
+   debounceTimer = setTimeout(function () {
+      const navbarTop = document.getElementById('navbar-top');
+      const currentScroll = window.scrollY;
 
-   if (hamburger && navLinks) {
-      hamburger.addEventListener('click', function () {
-         navLinks.classList.toggle('active');
-      });
-   }
+      if (currentScroll > 100 && lastScrollTop <= 100) {
+         // Ocultar con transición
+         navbarTop.style.transition = 'opacity 1s ease, height 1s ease';
+         navbarTop.style.opacity = '0';
+         navbarTop.style.height = '0'; 
+         navbarTop.style.visibility = 'hidden'; 
+      } else if (currentScroll <= 100 && lastScrollTop > 100) {
+         // Mostrar con transición
+         navbarTop.style.transition = 'opacity 1s ease, height 1s ease';
+         navbarTop.style.opacity = '1';
+         navbarTop.style.height = 'auto'; // Restablece su altura
+         navbarTop.style.visibility = 'visible'; // Lo vuelve visible
+      }
+
+      lastScrollTop = currentScroll;
+   }, 100); // Adjust the debounce delay as needed
 });
